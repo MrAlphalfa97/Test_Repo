0bb899c08284c5f529b1847c694a14c7749f583a
MrAlphalfa97 <32249785+MrAlphalfa97@users.noreply.github.com>
2018-11-07 18:10:59 -0800

    example 2
    see the new comments

--- test.java
+++ test.java
@@ -1,3 +1,5 @@
 public static void main(String[] args) {
   System.out.println("Hello world!");
 }
+// you can add whatever filetype you want i think
+// java, c or h, or php, or png, jpeg, whatever
